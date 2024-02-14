a37d77cc593a9b030a9125e74927493fde4fca48
kparker028 <60025945+kparker028@users.noreply.github.com>
2024-02-14 11:06:59 -0700

    chore: removed legacy queries.

--- src/Fetching/aboutMeData.tsx
+++ src/Fetching/aboutMeData.tsx
@@ -1,30 +1,5 @@
 import { graphqlClient } from "@/graphql/client";
 import { GetAboutMeDocument } from "@/graphql/generated/graphql";
 
-const getAboutMeData = async () => {
-	const query = `
-  query {
-  blogPage(where: {slug: "about-me"}){
-    title
-    description
-    slug
-    heroImage {
-      url
-    }
-  }
-}
-  `;
-	const body = { query };
-	const res = await fetch(process.env.HYGRAPH_API_URL, {
-		method: "POST",
-		body: JSON.stringify(body),
-	});
-	const { data } = await res.json();
-
-	return data.blogPage;
-};
-
-export default getAboutMeData;
-
 export const GetAboutMe = async () =>
 	await graphqlClient.request(GetAboutMeDocument);
--- src/Fetching/projectsData.tsx
+++ src/Fetching/projectsData.tsx
@@ -5,59 +5,6 @@ import {
 	GetProjectsDocument,
 } from "@/graphql/generated/graphql";
 
-export const getProjectsData = async () => {
-	const query = `
-  query {
-   projects {
-    projectName
-    description
-    projectImage {
-      url
-    }
-    slug
-  }
-}
-  `;
-	const body = { query };
-	const res = await fetch(process.env.HYGRAPH_API_URL, {
-		method: "POST",
-		body: JSON.stringify(body),
-	});
-	const { data } = await res.json();
-
-	return data.projects;
-};
-
-export const getProjectData = async (slug: string) => {
-	const query = `
-  query GetProjectBySlug($slug: String!) {
-	    project(where: {slug: $slug}) {
-	      projectName
-	      description
-	      projectImage {
-	        url
-	      }
-	    }
-    }
-	`;
-
-	const variables = {
-		slug,
-	};
-
-	const body = { query, variables };
-	const res = await fetch(process.env.HYGRAPH_API_URL, {
-		method: "POST",
-		body: JSON.stringify(body),
-	});
-	const { data, errors } = await res.json();
-
-	if (errors) {
-		return null;
-	}
-	return data.project;
-};
-
 export const getProjectDataWTypes = async (
 	variables: GetProjectBySlugQueryVariables
 ) => {
